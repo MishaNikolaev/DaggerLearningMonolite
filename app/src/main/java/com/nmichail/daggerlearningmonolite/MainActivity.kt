202c4d47393083b1b12d1ec7d58d73ded2c5d48e
Mikhail Nikolaev <nmichail154@mail.ru>
2026-05-03 23:34:15 +0700

    Initial commit

---
+++ app/src/main/java/com/nmichail/daggerlearningmonolite/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.nmichail.daggerlearningmonolite
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.nmichail.daggerlearningmonolite.ui.theme.DaggerLearningMonoliteTheme
+
+class MainActivity : ComponentActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			DaggerLearningMonoliteTheme {
+
+			}
+		}
+	}
+}
